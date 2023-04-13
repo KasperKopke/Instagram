825ec41cd103cec4e966958a3c56ca491c273e94
KasperKopke <kasper@kopke.info>
2023-04-13 14:14:09 +0200

    search er done og er i gang med notifikationer

--- js/nav.js
+++ js/nav.js
@@ -17,15 +17,41 @@ function navSearch() {
   if (magnifyingGlass.classList.contains("showSearch")) {
     document.querySelector(".main-navigation").style.maxWidth = "100px";
     document.querySelector(".main-navigation").style.transition =
-      "all 0.1s ease-in-out";
-    document.querySelector(".searchbar").style.transition =
-      "all 0.1s ease-in-out";
+      "all 0.3s ease-in-out";
   } else {
     document.querySelector(".main-navigation").style.maxWidth = "320px";
     document.querySelector(".main-navigation").style.transition =
-      "all 0.1s ease-in-out";
+      "all 0.3s ease-in-out";
   }
 }
+
+function notifikationer() {
+  const magnifyingGlass = document.querySelector(".fa-heart");
+  magnifyingGlass.classList.toggle("Noti");
+
+  const activeLogo = document.querySelector(".nav-insta-logo");
+  activeLogo.classList.toggle("active");
+  const activetext = document.querySelector(".nav-insta-text");
+  activetext.classList.toggle("active2");
+  const notiBar = document.querySelector(".notifikationer");
+  notiBar.classList.toggle("active4");
+
+  const meElements = document.querySelectorAll("#hideText");
+  for (let index = 0; index < meElements.length; index++) {
+    meElements[index].classList.toggle("hide");
+  }
+
+  if (magnifyingGlass.classList.contains("Noti")) {
+    document.querySelector(".main-navigation").style.maxWidth = "100px";
+    document.querySelector(".main-navigation").style.transition =
+      "all 0.3s ease-in-out";
+  } else {
+    document.querySelector(".main-navigation").style.maxWidth = "320px";
+    document.querySelector(".main-navigation").style.transition =
+      "all 0.3s ease-in-out";
+  }
+}
+
 /*
 denne kode gør så man kan lave dark mode den ser efter om searchbtn 
 indeholder classen showSearch hvis den gør skal den ændre baggrunds
