990ffa7a6bba71388ecfc019da0a848944ddb24f
dkanders12 <dkanders12@gmail.com>
2023-04-24 14:38:39 +0200

    new reels

--- js/nav.js
+++ js/nav.js
@@ -1,55 +1,84 @@
-function navSearch() {
-  const magnifyingGlass = document.querySelector(".fa-magnifying-glass");
-  magnifyingGlass.classList.toggle("showSearch");
+const beggeKnapper = document.querySelectorAll(".navli");
+console.log(beggeKnapper);
 
-  const activeLogo = document.querySelector(".nav-insta-logo");
-  activeLogo.classList.toggle("active");
-  const activetext = document.querySelector(".nav-insta-text");
-  activetext.classList.toggle("active2");
-  const searchbar = document.querySelector(".searchbar");
-  searchbar.classList.toggle("active3");
+const searchh = document.querySelector(".search");
+const notif = document.querySelector(".noti");
 
-  const meElements = document.querySelectorAll("#hideText");
-  for (let index = 0; index < meElements.length; index++) {
-    meElements[index].classList.toggle("hide");
-  }
+const sog = document.querySelector("#btns1");
+const notfifik = document.querySelector("#btns2");
 
-  if (magnifyingGlass.classList.contains("showSearch")) {
-    document.querySelector(".main-navigation").style.maxWidth = "100px";
-    document.querySelector(".main-navigation").style.transition =
-      "all 0.3s ease-in-out";
-  } else {
-    document.querySelector(".main-navigation").style.maxWidth = "320px";
-    document.querySelector(".main-navigation").style.transition =
-      "all 0.3s ease-in-out";
-  }
-}
+const searchbar = document.querySelector(".searchbar");
+const notiBar = document.querySelector(".notifikationer");
 
-function notifikationer() {
-  const magnifyingGlass = document.querySelector(".fa-heart");
-  magnifyingGlass.classList.toggle("Noti");
+const alleKnapper = document.querySelectorAll("#hideText");
 
-  const activeLogo = document.querySelector(".nav-insta-logo");
-  activeLogo.classList.toggle("active");
-  const activetext = document.querySelector(".nav-insta-text");
-  activetext.classList.toggle("active2");
-  const notiBar = document.querySelector(".notifikationer");
-  notiBar.classList.toggle("active4");
+const activeLogo = document.querySelector(".nav-insta-logo");
+const activetext = document.querySelector(".nav-insta-text");
 
-  const meElements = document.querySelectorAll("#hideText");
-  for (let index = 0; index < meElements.length; index++) {
-    meElements[index].classList.toggle("hide");
-  }
+for (let index = 0; index < beggeKnapper.length; index++) {
+  console.log(beggeKnapper[index]);
+  beggeKnapper[index].addEventListener("click", (data) => {
+    console.log(data.target);
+    if (sog.contains(data.target)) {
+      console.log("ja");
+      searchh.classList.toggle("active");
 
-  if (magnifyingGlass.classList.contains("Noti")) {
-    document.querySelector(".main-navigation").style.maxWidth = "100px";
-    document.querySelector(".main-navigation").style.transition =
-      "all 0.3s ease-in-out";
-  } else {
-    document.querySelector(".main-navigation").style.maxWidth = "320px";
-    document.querySelector(".main-navigation").style.transition =
-      "all 0.3s ease-in-out";
-  }
+      searchbar.classList.toggle("active");
+    } else {
+      console.log("nej");
+      searchh.classList.remove("active");
+
+      searchbar.classList.remove("active");
+    }
+
+    if (notfifik.contains(data.target)) {
+      console.log("ja");
+      notif.classList.toggle("active");
+
+      notiBar.classList.toggle("active");
+    } else {
+      console.log("nej");
+      notif.classList.remove("active");
+
+      notiBar.classList.remove("active");
+    }
+
+    if (
+      searchbar.classList.contains("active") ||
+      notiBar.classList.contains("active")
+    ) {
+      activeLogo.classList.add("active");
+      activetext.classList.add("active");
+    } else {
+      activeLogo.classList.remove("active");
+      activetext.classList.remove("active");
+    }
+
+    for (let index2 = 0; index2 < alleKnapper.length; index2++) {
+      console.log(alleKnapper[index2]);
+      if (
+        searchh.classList.contains("active") ||
+        notif.classList.contains("active")
+      ) {
+        alleKnapper[index2].classList.add("hide");
+      } else {
+        alleKnapper[index2].classList.remove("hide");
+      }
+    }
+
+    if (
+      searchh.classList.contains("active") ||
+      notif.classList.contains("active")
+    ) {
+      document.querySelector(".main-navigation").style.maxWidth = "100px";
+      document.querySelector(".main-navigation").style.transition =
+        "all 0.3s ease-in-out";
+    } else {
+      document.querySelector(".main-navigation").style.maxWidth = "320px";
+      document.querySelector(".main-navigation").style.transition =
+        "all 0.3s ease-in-out";
+    }
+  });
 }
 
 function opret() {
